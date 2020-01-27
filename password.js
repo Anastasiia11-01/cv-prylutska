fe07a19cf3d7a0a1aae3da7659dec07e70515669
Anastasiia11-01 <60342160+Anastasiia11-01@users.noreply.github.com>
2020-01-27 16:13:24 +0100

    Add files via upload

---
+++ password.js
@@ -0,0 +1,11 @@
+< script language = "JavaScript" >
+  <
+var password;
+var pass1 = "AnaPryl20";
+password = prompt('Enter Passwrod To View Page', '');
+if (password == pass1)
+  alert('Correct password, click OK to enter.');
+else {
+  window.location = "http://www.google.com";
+} </var>
+/script>
